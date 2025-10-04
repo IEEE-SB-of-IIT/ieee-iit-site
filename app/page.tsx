2c54d70aa964fc27f9afe4ccaba6f272998dcaf9
Danula <danusathmin@gmail.com>
2025-10-05 02:47:01 +0530

    feat: implement initial hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="min-h-screen">
+      <Hero />
+    </div>
+  );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Button from "./ui/button";
+
+export default function Hero() {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="Background Image" />
+      </div>
+
+      <div className="hero-layout">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <span className="text-white-50 md:text-3xl text-xl">
+                IEEE Student Branch of IIT
+              </span>
+              <div className="mt-5">
+                <h1>Advancing Technology</h1>
+                <h1> for Humanity</h1>
+              </div>
+            </div>
+            {/* <Button
+              text="Our Work"
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="counter"
+            /> */}
+          </div>
+        </header>
+      </div>
+    </section>
+  );
+}
